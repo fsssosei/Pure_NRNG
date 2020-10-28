f1aa3a3b8f3392808a375cea5bb43216c9e0f16f
fsssosei <43291778+fsssosei@users.noreply.github.com>
2020-10-29 01:07:32 +0800

    Create __init__.py

---
+++ src/rng_util_package/__init__.py
@@ -0,0 +1,21 @@
+'''
+rng_util - This is the common function package of random number generator.
+Copyright (C) 2020  sosei
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+from rng_util_package.rng_util_module import *
+
+version = '0.8.0'
