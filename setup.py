afb7c7ec2ab99c078bfaf2714e6f1ce413a127d9
fsssosei <43291778+fsssosei@users.noreply.github.com>
2020-10-29 01:05:46 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,45 @@
+'''
+pure_nrng - This package is used to generate non-deterministic random Numbers.
+Copyright (C) 2020  sosei
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+from setuptools import setup, find_packages
+
+with open("README.md", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='pure_nrng',
+    version='0.8.0',
+    description='Generate multi-precision non-deterministic random Numbers.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/fsssosei/Pure_NRNG',
+    license='GNU Affero General Public License v3',
+    author='sosei',
+    author_email='fss.sosei@gmail.com',
+    keywords=['NRNG', 'TRNG', 'Random', 'Multiple-precision'],
+    python_requires='>=3.8',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Programming Language :: Python :: 3.8',
+    ],
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    include_package_data=True,
+    install_requires=['gmpy2>=2.0.8']
+)
