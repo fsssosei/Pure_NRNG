237ebfeed9fa495ee4c77f8a1f5bb2420678ea44
fsssosei <43291778+fsssosei@users.noreply.github.com>
2020-11-05 14:24:33 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("README.md", encoding='utf-8') as fh:
 
 setup(
     name='pure_nrng',
-    version='0.8.2',
+    version='0.8.3',
     description='Generate multi-precision non-deterministic random Numbers.',
     long_description=long_description,
     long_description_content_type='text/markdown',
